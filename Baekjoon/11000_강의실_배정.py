d443349833fc0aebce22aefeab6d2b780cc2e965
진아영 <jin-ayeong@jin-ayeong-ui-MacBookAir.local>
2024-12-06 22:50:30 +0900

    solve: 백준 11000

--- Baekjoon/11000_강의실_배정.py
+++ Baekjoon/11000_강의실_배정.py
@@ -1,21 +1,20 @@
-import sys
 from heapq import heappush, heappop, heapify
 
-input = sys.stdin.readline
-
-N = int(input())
+N = int(input())  # 강의실 수
 lectures = [list(map(int, input().split())) for _ in range(N)]
 
-# heap 구조로 변경
-heapify(lectures)
+lectures.sort(key=lambda x: (x[0], x[1]))  # 강의실 정렬
 
-classroom = [0] * N
-cur_count = 0
+heap = []  # 강의 끝나는 시간 (빠른순)
 
-# for i in range(N):
-#
+for start, end in lectures:
 
+    # heap[0] <= 현재 강의 시작시간이라면
+    if heap and heap[0] <= start:
+        # 강의실 이어붙이기 (pop 하고 조건문 밖에서 새로 추가)
+        heappop(heap)
 
+    # 조건에 맞지 않으면 강의실 새로 추가
+    heappush(heap, end)
 
-print(classroom)
-print(cur_count)
+print(len(heap))
