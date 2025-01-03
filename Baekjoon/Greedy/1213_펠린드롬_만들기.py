3740fdac281712dbccde69416b38341cb98402b1
진아영 <ay8160@daum.net>
2025-01-03 14:56:42 +0900

    solve: 백준 1213

--- Baekjoon/Greedy/1213_펠린드롬_만들기.py
+++ Baekjoon/Greedy/1213_펠린드롬_만들기.py
@@ -12,6 +12,7 @@ for c in char:
 
     if c_count % 2 == 1:
 
+        # 홀수개인 문자 두번째 나타났을 때 : 팰린드롬 실패
         if odd != 0:
             print("I'm Sorry Hansoo")
             exit(0)
@@ -21,6 +22,7 @@ for c in char:
 
 answer = []
 
+# 팰린드롬 만들기
 for c in char:
 
     length = char_dic[c] // 2
