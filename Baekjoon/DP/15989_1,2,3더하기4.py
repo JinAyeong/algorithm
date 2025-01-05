5e1f7f33cc4f83d93622f12f6f8b839c1a276bab
진아영 <ay8160@daum.net>
2025-01-05 19:22:32 +0900

    solve: 백준 20125

--- Baekjoon/DP/15989_1,2,3더하기4.py
+++ Baekjoon/DP/15989_1,2,3더하기4.py
@@ -3,15 +3,14 @@
 # 3 : 3가지 (3, 2+1, 1+1+1)
 # 4 : 4가지 (3+1, 2+1+1, 1+1+1+1, 2+2)
 
-
-dp = [0] * (1000001)
+dp = [0] * (10001)
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
-for i in range(4, 1000001):
-    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009
+for i in range(4, 10001):
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3])
 
 for _ in range(int(input())):
     n = int(input())
-    print(dp[n] % 1000000009)
+    print[dp[n]]
