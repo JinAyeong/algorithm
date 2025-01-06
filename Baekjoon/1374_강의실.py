b2e398a57ea1444ff3d16d8acfa82c4ad62e32e3
진아영 <ay8160@daum.net>
2025-01-06 20:42:36 +0900

    solve: 백준 12101

---
+++ Baekjoon/1374_강의실.py
@@ -0,0 +1,5 @@
+from heapq import heappush, heappop, heapify
+
+# 최소 필요한 강의실 수 출력
+
+N = int(input())
---
+++ Baekjoon/DP/12101_1,2,3더하기2.py
@@ -0,0 +1,24 @@
+n, k = map(int, input().split())
+
+dp = [[] for i in range(12)]
+dp[0] = ['']
+dp[1] = ['1']
+dp[2] = ['1+1', '2']
+dp[3] = ['1+1+1', '1+2', '2+1', '3']
+
+for i in range(4, n+1):
+
+    for j in range(len(dp[i-1])):
+        dp[i].append(dp[i-1][j] + '+1')
+
+    for j in range(len(dp[i-2])):
+        dp[i].append(dp[i-2][j] + '+2')
+
+    for j in range(len(dp[i-3])):
+        dp[i].append(dp[i-3][j] + '+3')
+
+try:
+    dp[n].sort()
+    print(dp[n][k-1])
+except:
+    print(-1)
