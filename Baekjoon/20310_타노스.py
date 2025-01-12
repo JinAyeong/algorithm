f681ecc9379b8f0b7b22f81a2744ac062dae6b56
진아영 <ay8160@daum.net>
2025-01-12 20:30:44 +0900

    solve: 백준 18428

--- Baekjoon/20310_타노스.py
+++ Baekjoon/20310_타노스.py
@@ -1,3 +1,5 @@
+# 문자열의 순서 바꾸면 안됨 -> 주어진 순서에서 pop 하듯이 삭제 가능
+
 from collections import defaultdict
 
 S = input()
@@ -6,6 +8,3 @@ char = defaultdict(int)
 for s in S:
     char[s] += 1
 
-result = '0' * (char['0'] // 2) + '1' * (char['1'] // 2)
-
-print(result)
