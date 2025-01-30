d3263846cbb39619f0f26402fbc21a73146a6b67
진아영 <ay8160@daum.net>
2025-01-30 22:36:12 +0900

    refactor: [BOJ] 2470 / 두 용액 / 골드4

--- Baekjoon/TwoPointer/2470_두_용액.py
+++ Baekjoon/TwoPointer/2470_두_용액.py
@@ -11,21 +11,19 @@ while left < right:
 
     solution = solutions[left] + solutions[right]
 
+    # 결과 먼저 갱신
+    if abs(solution) < value:
+        result = [solutions[left], solutions[right]]
+        value = abs(solution)
+
+    # 0에 가까워지도록 포인터 조절
     if solution < 0:
-        if abs(solution) < value:
-            result = [solutions[left], solutions[right]]
-            value = abs(solution)
         left += 1
 
     elif solution > 0:
-        if abs(solution) < value:
-            result = [solutions[left], solutions[right]]
-            value = abs(solution)
         right -= 1
 
     elif solution == 0:
-        result = [solutions[left], solutions[right]]
-        value = abs(solution)
         break
 
 print(*result)
