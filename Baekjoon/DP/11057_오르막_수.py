828561a11269075d0ce5dd2e6d102a41451638da
진아영 <ay8160@daum.net>
2025-02-04 21:44:05 +0900

    refactor: [BOJ] 11057 / 오르막 수 / 실버1

--- Baekjoon/DP/11057_오르막_수.py
+++ Baekjoon/DP/11057_오르막_수.py
@@ -5,7 +5,12 @@ dp = [[0] * 10 for _ in range(N+1)] # dp[i][j] : i자리수에서 첫 숫자가 
 dp[1] = [1] * 10
 
 for i in range(2, N+1):
-    for j in range(10):
-        dp[i][j] = (sum(dp[i-1][j:])) % 10007
+    # for j in range(10):
+    #     dp[i][j] = (sum(dp[i-1][j:])) % 10007
+
+    # 누적합으로 변경
+    dp[i][9] = dp[i-1][9]
+    for j in range(8, -1, -1):
+        dp[i][j] = (dp[i-1][j] + dp[i][j+1]) % 10007
 
 print(sum(dp[N]) % 10007)
