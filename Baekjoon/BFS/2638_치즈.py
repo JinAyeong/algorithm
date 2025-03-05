bfab09a6436cd8090e8fd8861cd6c1563707f613
진아영 <ay8160@daum.net>
2025-03-05 15:10:40 +0900

    refactor: [BOJ] 2638 / 치즈 / 골드3

--- Baekjoon/BFS/2638_치즈.py
+++ Baekjoon/BFS/2638_치즈.py
@@ -5,6 +5,69 @@
 2. 먼저 공기 닿은 부분 먼저 탐색
 3. 그 후로 치즈 배열 반복하면서 녹이기
 '''
+
+from collections import deque
+
+N, M = map(int, input().split()) # 세로, 가로
+mp = [list(map(int, input().split())) for _ in range(N)]
+
+# 외부 공기에 닿은 치즈 녹이기
+def outside():
+    visited = [[False] * M for _ in range(N)]
+    visited[0][0] = True
+    mp[0][0] = -1
+    q = deque([(0, 0)])
+
+    while q:
+        i, j = q.popleft()
+
+        for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            ni, nj = i + di, j + dj
+
+            if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and mp[ni][nj] != 1:
+                mp[ni][nj] = -1
+                q.append((ni, nj))
+                visited[ni][nj] = True
+
+def melting():
+
+    melting_cheese = []
+
+    for i in range(N):
+        for j in range(M):
+            if mp[i][j] == 1:
+                cnt = 0
+
+                for di, dj in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+                    ni, nj = i + di, j + dj
+
+                    if 0 <= ni < N and 0 <= nj < M and mp[ni][nj] == -1:
+                        cnt += 1
+
+                if cnt >= 2:
+                    melting_cheese.append((i, j))
+
+    if not melting_cheese:
+        return False
+
+    for x, y in melting_cheese:
+        mp[x][y] = 0
+
+    return True
+
+result = 0
+
+while True:
+    outside()
+    if not melting():
+        break
+    result += 1
+
+print(result)
+
+
+#--------------------------------------------------------------------------------------
+# 최적화 전
 from collections import deque
 
 N, M = map(int, input().split()) # 세로, 가로
