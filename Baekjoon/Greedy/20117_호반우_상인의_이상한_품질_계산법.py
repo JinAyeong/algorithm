e1fe81c6faa26c3b6982d1f8bb5edfdeea582fb5
진아영 <ay8160@daum.net>
2025-03-06 12:15:40 +0900

    solve: [BOJ] 20117 / 호반우 상인의 이상한 품질 계산법 / 실버1

--- Baekjoon/Greedy/20117_호반우_상인의_이상한_품질_계산법.py
+++ Baekjoon/Greedy/20117_호반우_상인의_이상한_품질_계산법.py
@@ -1,16 +1,16 @@
 '''
 묶음이 짝수 -> 중앙값: (묶음개수 / 2 + 1)
 묶음이 홀수 -> 중앙값: ((묶음개수 + 1) / 2)
+길이가 2인 묶음으로 다 묶기
 '''
-
-def middle(n):
-    if n % 2 == 0:
-        return (n // 2) + 1
-    else:
-        return (n + 1) // 2
-
 
 N = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 
+result = sum(arr[(N+1)//2:]) * 2
+
+if N % 2 == 1:
+    result += arr[N//2]
+
+print(result)
