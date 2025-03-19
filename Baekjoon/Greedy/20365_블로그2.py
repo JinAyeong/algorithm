36c0db2e064e5f2e2dc3f431777a5d493412339b
진아영 <ay8160@daum.net>
2025-03-20 02:51:22 +0900

    refactor: [BOJ] 20365 / 블로그2 / 실버3

--- Baekjoon/Greedy/20365_블로그2.py
+++ Baekjoon/Greedy/20365_블로그2.py
@@ -1,3 +1,30 @@
+# 투포인터
+# python 33392 KB 180 ms
+
+N = int(input())
+colors = input()
+left, right = 0, N - 1
+result = 0
+
+while left <= right:
+    if colors[left] == colors[right]:
+        char = colors[left]
+        while left <= right and colors[left] == char:
+            left += 1
+        while left <= right and colors[right] == char:
+            right -= 1
+    else:
+        char = colors[right]
+        while left <= right and colors[right] == char:
+            right -= 1
+
+    result += 1
+
+print(result)
+
+#-------------------------------------------------------
+# 문자열
+# python 33392 KB 1756 ms
 N = int(input())
 colors = input()
 result = 0
