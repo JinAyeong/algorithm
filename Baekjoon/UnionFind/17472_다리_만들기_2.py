31bab37375ab59f6f27d92406f5311deeeedd7a2
진아영 <ay8160@daum.net>
2025-04-26 03:09:52 +0900

    refactor: [BOJ] 17472 / 다리 만들기 2 / 골드1

--- Baekjoon/UnionFind/17472_다리_만들기_2.py
+++ Baekjoon/UnionFind/17472_다리_만들기_2.py
@@ -17,113 +17,96 @@
     a. 후보 길이순으로 정렬
     b. 다리 하나 꺼내서 시작과 끝 다리 연결 (union)
 4. 다리 연결 후 하나로 이루어져 있는지 검사 (find)
-
 '''
 from collections import deque
 
 N, M = map(int, input().split())
 mp = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
-island = deque([])
-direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
-bridge = []
+directions = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 
-# 섬 끝부분 찾기
-def find_island(x, y, num):
+visited = [[False] * M for _ in range(N)]
+island_edges = deque()  # 섬의 가장자리 좌표들 저장
+bridges = []            # 가능한 다리 후보들 저장
+
+# 1. 하나의 섬을 탐색하여 번호를 붙이고, 가장자리를 저장
+def mark_island(x, y, island_id):
     q = deque([(x, y)])
     visited[x][y] = True
-    mp[x][y] = num
+    mp[x][y] = island_id
 
     while q:
         cx, cy = q.popleft()
-        is_edge = False
 
-        for dx, dy in direction:
+        for dx, dy in directions:
             nx, ny = cx + dx, cy + dy
 
-            if 0 <= nx < N and 0 <= ny < M:
-                if not visited[nx][ny]:
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
                     if mp[nx][ny] == 1:
                         visited[nx][ny] = True
-                        mp[nx][ny] = num
+                        mp[nx][ny] = island_id
                         q.append((nx, ny))
                     elif mp[nx][ny] == 0:
-                        is_edge = True
-            else:
-                is_edge = True
+                        island_edges.append((cx, cy, island_id))
 
-        if is_edge:
-            island.append((cx, cy, num))
-
-
-# 섬 끝부분에서 다른 섬으로 연결하기
-def build(x, y, num, d):
+# 2. 한 방향으로만 다리를 건설하며 다른 섬에 도달할 수 있는지 확인
+def build_bridge(x, y, island_id, direction_index):
+    dx, dy = directions[direction_index]
     length = 0
-    cx, cy = x, y
+    nx, ny = x + dx, y + dy
 
-    while True:
-        cx += direction[d][0]
-        cy += direction[d][1]
-
-        if not (0 <= cx < N and 0 <= cy < M):
+    while 0 <= nx < N and 0 <= ny < M:
+        if mp[nx][ny] == island_id:  # 자기 섬 만나면 중단
             break
-
-        if mp[cx][cy] == num:
-            break
-
-        if mp[cx][cy] != 0 and mp[cx][cy] != num:
+        if mp[nx][ny] != 0 and mp[nx][ny] != island_id:
             if length >= 2:
-                bridge.append((length, num, mp[cx][cy]))
+                bridges.append((length, island_id, mp[nx][ny]))
             break
+        # 바다인 경우 계속 진행
+        length += 1
+        nx += dx
+        ny += dy
 
-        if mp[cx][cy] == 0:
-            length += 1
-
-# 섬 찾기
-cnt = 2
+# 3. 모든 섬을 탐색하면서 번호 부여 및 가장자리 수집
+island_id = 2  # 섬 번호는 2부터 시작
 for i in range(N):
     for j in range(M):
-        if not visited[i][j] and mp[i][j]:
-            visited[i][j] = True
-            find_island(i, j, cnt)
-            cnt += 1
+        if not visited[i][j] and mp[i][j] == 1:
+            mark_island(i, j, island_id)
+            island_id += 1
 
-
-# 섬 연결하기
-for x, y, num in island:
+# 4. 각 가장자리에서 다리 건설 시도
+for x, y, id in island_edges:
     for d in range(4):
-        build(x, y, num, d)
+        build_bridge(x, y, id, d)
 
-parent = [i for i in range(cnt)]
+# 5. 다리 연결확인 및 연결하기
+parent = [i for i in range(island_id)]
 
 def find(a):
-
     if parent[a] != a:
         parent[a] = find(parent[a])
     return parent[a]
 
 def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+    a_root = find(a)
+    b_root = find(b)
+    if a_root != b_root:
+        if a_root < b_root:
+            parent[b_root] = a_root
+        else:
+            parent[a_root] = b_root
 
-# 다리 연결
-bridge.sort()
-result = 0
-count = 0
-for length, a, b in bridge:
+# 6. 다리 연결
+bridges.sort()
+total_length = 0
+connection_count = 0
+
+for length, a, b in bridges:
     if find(a) != find(b):
         union(a, b)
-        result += length
-        count += 1
+        total_length += length
+        connection_count += 1
 
-# 섬 연결 확인
-roots = set()
-
-for idx in range(2, cnt):
-    roots.add(find(idx))
-
-print(result if len(roots) == 1 else -1)
+# 7. 모든 섬이 연결되어 있는지 확인
+roots = set(find(i) for i in range(2, island_id))
+print(total_length if len(roots) == 1 else -1)
