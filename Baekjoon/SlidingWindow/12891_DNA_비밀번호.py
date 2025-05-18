e5dbdd8ac7633ac180a21196c982e70b975e069c
진아영 <ay8160@daum.net>
2025-05-18 19:49:16 +0900

    refactor: [BOJ] 12891 / DNA 비밀번호 / 실버2

--- Baekjoon/SlidingWindow/12891_DNA_비밀번호.py
+++ Baekjoon/SlidingWindow/12891_DNA_비밀번호.py
@@ -1,31 +1,48 @@
-S, P = map(int, input().split())
-dna = input()
-A, C, G, T = map(int, input().split())
+class DnaPasswordChecker:
+    def __init__(self, S, P, dna, required_counts):
+        self.S = S
+        self.P = P
+        self.dna = dna
+        self.required = {
+            'A': required_counts[0],
+            'C': required_counts[1],
+            'G': required_counts[2],
+            'T': required_counts[3]
+        }
+        self.current_count = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+        self.result = 0
 
-dna_cnt = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    def is_valid(self):
+        for nucleotide in 'ACGT':
+            if self.current_count[nucleotide] < self.required[nucleotide]:
+                return False
+        return True
 
-def check(dict):
+    def add(self, char):
+        self.current_count[char] += 1
 
-    if dict['A'] < A:
-        return False
-    if dict['C'] < C:
-        return False
-    if dict['G'] < G:
-        return False
-    if dict['T'] < T:
-        return False
-    return True
+    def remove(self, char):
+        self.current_count[char] -= 1
 
-result = 0
+    def check_passwords(self):
+        for i in range(self.S):
+            self.add(self.dna[i])
+            if i >= self.P:
+                self.remove(self.dna[i - self.P])
+            if i >= self.P - 1 and self.is_valid():
+                self.result += 1
+        return self.result
 
-for i in range(S):
 
-    dna_cnt[dna[i]] += 1
+def main():
+    S, P = map(int, input().split())
+    dna = input()
+    required_counts = list(map(int, input().split()))
 
-    if i >= P:
-        dna_cnt[dna[i-P]] -= 1
+    checker = DnaPasswordChecker(S, P, dna, required_counts)
+    result = checker.check_passwords()
+    print(result)
 
-    if i >= P-1 and check(dna_cnt):
-        result += 1
 
-print(result)
+if __name__ == "__main__":
+    main()
