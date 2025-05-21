fd4fbffd166db22bb118292ced59b980625ae9b6
진아영 <ay8160@daum.net>
2025-05-21 16:26:57 +0900

    solve: [BOJ] 20061 / 모노미노도미노 2 / 골드2

--- Baekjoon/20061_모노미노도미노_2.py
+++
@@ -1,8 +0,0 @@
-N = int(input())
-
-red = [[0] * 4 for _ in range(4)]
-blue = [[0] * 6 for _ in range(4)]
-green = [[0] * 4 for _ in range(6)]
-
-for _ in range(N):
-    t, x, y = map(int, input().split())
