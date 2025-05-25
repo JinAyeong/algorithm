9892a655af4a5f4bb7dd733055bdb7c087dac2f6
진아영 <ay8160@daum.net>
2025-05-26 00:15:56 +0900

    solve: [BOJ] 2527 / 직사각형 / 실버1

---
+++ Baekjoon/Implementation/2527_직사각형.py
@@ -0,0 +1,14 @@
+for _ in range(4):
+    x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
+
+    x_overlap = min(p1, p2) - max(x1, x2)
+    y_overlap = min(q1, q2) - max(y1, y2)
+
+    if x_overlap < 0 or y_overlap < 0:
+        print('d')
+    elif x_overlap == 0 and y_overlap == 0:
+        print('c')
+    elif x_overlap == 0 or y_overlap == 0:
+        print('b')
+    else:
+        print('a')
