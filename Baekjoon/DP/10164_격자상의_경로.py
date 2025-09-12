3686b8f30e81ef2bcc5cd5271b4f724dc3137e71
jinayeong <ay8160@daum.net>
2025-09-12 12:29:00 +0900

    todo: [BOJ] 10164 / 격자상의 경로 / 실버2

---
+++ Baekjoon/DP/10164_격자상의_경로.py
@@ -0,0 +1,21 @@
+N, M, K = map(int, input().split())
+
+dp = [[0] * (M + 1) for _ in range(N + 1)]
+dp[1][1] = 1
+
+a = (K // M + 1 if K else N)
+b = (K % M if K else M)
+
+for i in range(1, a+1):
+    for j in range(1, b+1):
+        if (i, j) == (1, 1):
+            continue
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+for k in range(a, N+1):
+    for l in range(b, M+1):
+        if (k, l) == (a, b):
+            continue
+        dp[k][l] = dp[k-1][l] + dp[k][l-1]
+
+print(dp[N][M])
