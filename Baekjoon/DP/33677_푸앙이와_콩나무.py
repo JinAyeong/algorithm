8464662fa652e5dc2cc335ee3dd6828ba072e9f1
jinayeong <ay8160@daum.net>
2025-10-15 12:48:14 +0900

    refactor: [BOJ] 33677 / 푸앙이와 콩나무 / 실버1

--- Baekjoon/DP/33677_푸앙이와_콩나무.py
+++ Baekjoon/DP/33677_푸앙이와_콩나무.py
@@ -4,8 +4,45 @@
 5: **2
 '''
 
+# python 48040 KB, 764 ms
+
 N = int(input())
-dp = [[float('inf'), 0]] * (N+1) # 일수, 물의 양
+dp_water = [float('inf')] * (N+1)
+dp_day = [float('inf')] * (N+1)
+dp_water[0] = dp_day[0] = 0
+
+def update(cur, next_pos, water_add):
+    if next_pos >= N+1:
+        return
+    
+    day = dp_day[cur] + 1
+    water = dp_water[cur] + water_add
+
+    if (dp_day[next_pos] > day or
+        (dp_day[next_pos] == day and dp_water[next_pos] > water)):
+        dp_day[next_pos] = day
+        dp_water[next_pos] = water
+
+for i in range(N):
+
+    day = dp_day[i] + 1
+
+    # 물 1만큼 주기
+    update(i, i+1, 1)
+    
+    # 물 3만큼 주기
+    update(i, i*3, 3)
+
+    # 물 5만큼 주기
+    update(i, i**2, 5)
+
+print(dp_day[N], dp_water[N])
+
+#########################################################################
+# python 120096 KB, 1124 ms
+
+N = int(input())
+dp = [(float('inf'), 0)] * (N+1) # 일수, 물의 양
 dp[0] = (0, 0)
 
 for i in range(N):
