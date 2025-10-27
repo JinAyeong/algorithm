c0a16e7605b9fca9b0128ecd29263d1fb2d14426
jinayeong <ay8160@daum.net>
2025-10-27 12:55:23 +0900

    refactor: [BOJ] [PCCP 기출문제] 10번 공원 / Lv1

--- programmers/PrefixSum/Lv1_[PCCP기출문제]_10번공원.py
+++ programmers/PrefixSum/Lv1_[PCCP기출문제]_10번공원.py
@@ -3,21 +3,19 @@
 def solution(mats, park):
     mats.sort(reverse=True)
     r, c = len(park), len(park[0])
-
-    # -1 → 0, 나머지 → 1로 변환
     grid = [[0 if park[i][j] == '-1' else 1 for j in range(c)] for i in range(r)]
 
-    # 누적합 배열 (1-based index)
-    S = [[0]*(c+1) for _ in range(r+1)]
+    # 누적합 배열
+    prefix = [[0]*(c+1) for _ in range(r+1)]
     for i in range(1, r+1):
         for j in range(1, c+1):
-            S[i][j] = grid[i-1][j-1] + S[i-1][j] + S[i][j-1] - S[i-1][j-1]
+            prefix[i][j] = grid[i-1][j-1] + prefix[i-1][j] + prefix[i][j-1] - prefix[i-1][j-1]
 
     # 매트 크기별 검사
     for mat in mats:
         for i in range(r - mat + 1):
             for j in range(c - mat + 1):
-                total = S[i+mat][j+mat] - S[i][j+mat] - S[i+mat][j] + S[i][j]
+                total = prefix[i+mat][j+mat] - prefix[i][j+mat] - prefix[i+mat][j] + prefix[i][j]
                 if total == 0:
                     return mat
     return -1
