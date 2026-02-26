656f30cae29a63f9ec8912443692f06bb9394cfa
jinayeong <ay8160@daum.net>
2026-02-26 12:29:02 +0900

    todo: [BOJ] 11567 / 선진이의 겨울 왕국 / 골드3

--- Baekjoon/11567_선진이의_겨울_왕국.py
+++ Baekjoon/11567_선진이의_겨울_왕국.py
@@ -9,38 +9,41 @@ from collections import deque
 
 n, m = map(int, input().split())
 mp = [list(input()) for _ in range(n)]
+
 r1, c1 = map(lambda x: int(x) - 1, input().split())
 r2, c2 = map(lambda x: int(x) - 1, input().split())
 
+visited = [[False] * m for _ in range(n)]
+visited[r1][c1] = True
+
 q = deque([(r1, c1)])
-mp[r1][c1] = 'X'
 
 def solve():
-    if (r1, c1) == (r2, c2):
-        return 'YES'
     
     while q:
         cr, cc = q.popleft()
 
-        if (cr, cc) == (r2, c2):
-
-            for dr, dc in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-                nr, nc = cr + dr, cc + dc
-
-                if 0 <= nr < n and 0 <= nc < m and mp[nr][nc] == '.':
-                    return 'YES'
-            return 'NO'
-
         for dr, dc in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
             nr, nc = cr + dr, cc + dc
 
-            if 0 <= nr < n and 0 <= nc < m and mp[nr][nc] == '.':
-                mp[nr][nc] = 'X'
-                q.append((nr, nc))
-            elif 0 <= nr < n and 0 <= nc < m and mp[nr][nc] == 'X' and (nr, nc) == (r2, c2):
-                return 'YES'
+            if 0 <= nr < n and 0 <= nc < m:
+
+                # 도착
+                if (nr, nc) == (r2, c2):
+                    if mp[r2][c2] == 'X':
+                        return 'YES'
+                    else:
+                        for dr2, dc2 in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                            nr2, nc2 = r2 + dr2, c2 + dc2
+                            if 0 <= nr2 < n and 0 <= nc2 < m and mp[nr2][nc2] == '.' and not visited[nr2][nc2]:
+                                return 'YES'
+                        continue
+                
+                # 진행
+                if mp[nr][nc] == '.' and not visited[nr][nc]:
+                    visited[nr][nc] = True
+                    q.append((nr, nc))
 
     return 'NO'
 
-answer = solve()
-print(answer)
+print(solve())
