39ffa0338a4db4175e44f182f69369ce4bdaca36
jinayeong <ay8160@daum.net>
2026-07-08 12:33:33 +0900

    solve: [PGS] 짝지어 제거하기 / Lv2

---
+++ programmers/Structures/Lv2_짝지어_제거하기.py
@@ -0,0 +1,17 @@
+def solution(s):
+
+    stack = []
+    
+    for char in s:
+        
+        if not stack:
+            stack.append(char)
+        elif stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+            
+    if not stack:
+        return 1
+
+    return 0
