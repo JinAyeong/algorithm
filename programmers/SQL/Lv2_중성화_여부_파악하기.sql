699f22846a4cf045f655db93e497c4e06d85b322
jinayeong <ay8160@daum.net>
2026-07-08 12:56:35 +0900

    solve: [PGS] 중성화 여부 파악하기 / Lv2

--- programmers/SQL/Lv2_중성화_여부_파악하기.sql
+++ programmers/SQL/Lv2_중성화_여부_파악하기.sql
@@ -1,10 +1,12 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID,
-       NAME,
-       # WHERE : 특정 절 만족하는 행 선택, CASE : 조건에 따라 다른 값 반환
-       CASE
-            WHEN (SEX_UPON_INTAKE LIKE '%Neutered%') THEN 'O'
-            WHEN (SEX_UPON_INTAKE LIKE '%Spayed%') THEN 'O'
-            ELSE 'X'
-        END AS 중성화
+SELECT
+    ANIMAL_ID,
+    NAME,
+    CASE
+        WHEN (
+            SEX_UPON_INTAKE LIKE '%Neutered%'
+             OR SEX_UPON_INTAKE LIKE '%Spayed%'
+        ) THEN 'O'
+        ELSE 'X'
+    END AS 중성화
 FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
