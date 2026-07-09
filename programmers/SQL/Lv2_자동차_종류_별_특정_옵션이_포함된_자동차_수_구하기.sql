c6d3d1634017a0f60820071571baa53c425a15dc
jinayeong <ay8160@daum.net>
2026-07-10 08:58:22 +0900

    solve: [PGS] 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기 / Lv2

--- programmers/SQL/Lv2_자동차_종류_별_특정_옵션이_포함된_자동차_수_구하기.sql
+++ programmers/SQL/Lv2_자동차_종류_별_특정_옵션이_포함된_자동차_수_구하기.sql
@@ -1,9 +1,11 @@
 SELECT CAR_TYPE, COUNT(*) AS CARS
 FROM CAR_RENTAL_COMPANY_CAR
 -- where 절에서는 = 연사자 사용 불가능
-WHERE OPTIONS LIKE '%가죽시트%'
-   OR OPTIONS LIKE '%열선시트%'
-   OR OPTIONS LIKE '%통풍시트%'
+WHERE (
+    OPTIONS LIKE '%통풍시트%'
+    OR OPTIONS LIKE '%열선시트%'
+    OR OPTIONS LIKE '%가죽시트%'
+)
 -- GROUP BY : 차 종류별로 묶기
 GROUP BY CAR_TYPE 
 ORDER BY CAR_TYPE;
